d180cb2d3cf138823854a88a69fcfbe0eb6cc577
manoj-ojha04 <manojojha154@gmail.com>
2026-01-07 15:57:17 +0530

    add backend url

--- server/server.js
+++ server/server.js
@@ -10,7 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors({}));
+app.use(cors({
+    origin: "https://resumebuilder-mu-ecru.vercel.app",
+    methods: ['GET','POST','PUT','DELETE','PATCH','OPTIONS'], // array में uppercase method names
+}));
+
+
 
 dbConnected();
 
